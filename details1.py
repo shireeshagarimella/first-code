2f31868810e529a54cb2b07ef53d943218ae4d92
shireeshagarimella <garimella.shireesha@gmail.com>
2019-06-03 18:08:45 +0530

    fixed file size with random string creation

--- details1.py
+++ details1.py
@@ -1,17 +1,13 @@
-f = open("E:\\Python\\own practice\\details1.txt","r")
-x = f.read()
-f.close()
-s = x.split('\n')
-k = []
-for l in s:
-    i = input(l)
-    k.append(i)
-y = ':'
 j = ['name', 'birth date', 'gender', 'phone no', 'email id', 'password']
+k = {}
+for l in j:
+    i = input("please enter your {} :". format(l))
+    k[l] = i
+
 f = open("E:\\Python\\own practice\\bio data.txt", "w+")
 f.write(f'{"profile".upper()}' '\n')
-for a, b in zip(k, j):
-    f.write(f'{b.upper():<20}{y:<20}{a.upper():<20}''\n')
+for a in k:
+    f.write(f'{a.upper():<30}{":":<20}{k[a].upper():<20}''\n')
 f.seek(0)
 op = f.read()
 f.close()

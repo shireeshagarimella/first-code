03f2d06384ca10fe9fa0ccb3b3a905836899bfa4
shireeshagarimella <garimella.shireesha@gmail.com>
2019-06-13 11:22:34 +0530

    creating files with random string

--- directory creation.py
+++ directory creation.py
@@ -2,16 +2,21 @@ import os
 import datetime
 import random
 import string
-print("slno".ljust(10, " "), "file name".ljust(40, " "), "created at".ljust(60, " "), "directory".ljust(60, " "))
+
+y = {}
+q = os.getcwd()
+x = len([name for name in os.listdir(q) if os.path.isfile(os.path.join(q, name))])
 for n in range(10):
     now = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
-    q = os.getcwd()
     p = q + "\\" + now + ".txt"
-    x = len([name for name in os.listdir(q) if os.path.isfile(os.path.join(q, name))])
-    num_chars = 104857600
     fn = open(p, 'w+')
-    while os.path.getsize(p) < num_chars:
+    while os.path.getsize(p) < 10485760:
         str_ran = ''.join(random.choices(string.ascii_letters + string.digits, k=512))
         fn.write(str_ran)
         os.path.getsize(p)
-    print(str(x+1).ljust(10, " "), now.ljust(40, " "), q.ljust(60, " "), p.ljust(60, " "))
+    y[now] = p
+print("sl no".ljust(10, " "), "file name".ljust(40, " "), "created at".ljust(60, " "), "directory".ljust(60, " "))
+c = 1
+for a in y:
+    print(str(x + c).ljust(10, " "), a.ljust(40, " "), q.ljust(60, " "), y[a].ljust(60, " "))
+    c = c + 1
